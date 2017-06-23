d7a2703a72a48f1dd6b598443f19c8d32774a95f
zachglassman <zach.glassman@gmail.com>
2017-06-23 16:50:48 -0400

    more docs

--- simtools/Calculation.py
+++ simtools/Calculation.py
@@ -10,6 +10,7 @@ try:
 except:
     _PARALLEL = False
 
+# TODO Move hdf5 file preparation into simulation not calculation
 
 class Calculation(object):
     """function should return dictionary"""
@@ -22,20 +23,50 @@ class Calculation(object):
         self._prepare_hdf_file(overwrite_file)
 
     def _prepare_hdf_file(self, overwrite_file):
-        """prepare hdf5file"""
+        """prepare hdf5file
+        
+        Parameters
+        ----------
+        overwrite_file : bool
+            overwrite the hdf5 file upon prepration of calculation
+            usually 
+        """
         if overwrite_file or not os.path.isfile(self._filepath):
             with h5py.File(self._filepath, 'w') as file_:
                 pass
 
     def get_function_args(self):
+        """get the fileath and id of Calculation
+        
+        Returns
+        -------
+        args : list 
+             list of names of argments
+        """
         return self._args
 
     def set_args(self, filepath, id_):
+        """set filepath and id of Calculation
+
+        Parameters
+        ----------
+        filepath : str
+            filepath of calculation results file
+        
+        id_ : any 
+            indentifier for calculation (usually a number)
+        """
         self._filepath = filepath
         self._id = id_
 
     def add_params(self, params):
-        """add ParameterGroup to simulation and validate its inputs"""
+        """add ParameterGroup to simulation and validate its inputs
+        
+        Parameters
+        ----------
+        params : ParameterGroup
+            ParameterGropu to add to the calculation
+        """
         assert isinstance(
             params, ParameterGroup), "Parameters must be of type Parameter group"
         # check if arguments are in parameters (can be extra)
@@ -45,6 +76,18 @@ class Calculation(object):
         self._params = params
 
     def _generate_params(self, expansion_type):
+        """generate parameter expansions
+
+        Parameters
+        ----------
+        expansion_type : str 
+            Type of expansion, options are single, zip, outer
+        
+        Returns
+        -------
+        param_list : list 
+            list of dictionaries of parameters
+        """
         if expansion_type == 'zip':
             param_list = self._params.zip()
         elif expansion_type == 'outer':
@@ -55,7 +98,24 @@ class Calculation(object):
         return param_list
 
     def run(self, expansion_type=None, parallel=False, n_jobs=4):
-        """run the calculation and put into result object"""
+        """run the calculation and put into result object
+        
+        Parameters
+        ----------
+        expansion_type : str 
+            Type of expansion, options are single, zip, outer
+        
+        parallel : bool
+            parallelize calculation over cores
+        
+        n_jobs : int 
+            number of cores to use for parallization
+
+        Returns
+        -------
+        df : pd.DataFrame
+            result of _process_results function
+        """
         param_list = self._generate_params(expansion_type)
         if parallel and _PARALLEL:
             answer = Parallel(n_jobs=n_jobs)(delayed(self._func)(**i)
@@ -66,7 +126,21 @@ class Calculation(object):
 
     def _process_results(self, params, ans):
         """process results, save to hdf5 file
-        build dataframe with parameters and file_paths"""
+        build dataframe with parameters and file_paths
+        
+        Parameters
+        ----------
+        params : list 
+           list of dictionaries which hold parameters
+        ans : list 
+           list of dictionary results from computation
+
+        Returns
+        -------
+        df : pd.DataFrame 
+           dataframe containing information in parameters and indexes
+           into the hdf5 file where where the simulation is stored
+        """
         d_to_dataframe = []
         with h5py.File(self._filepath) as file_:
             calc_group = file_.create_group(str(self._id))
@@ -79,5 +153,5 @@ class Calculation(object):
                 for k, v in r.items():
                     dataset = group.create_dataset(k, data=np.array(v))
                 d_to_dataframe.append(temp)
-
-        return pd.DataFrame(d_to_dataframe)
+        df = pd.DataFrame(d_to_dataframe)
+        return df
