ee5f9d91be7f7079084b93c617813a4479af8482
zachglassman <zach.glassman@gmail.com>
2017-06-23 12:27:44 -0400

    edit the readme

--- README.rst
+++ README.rst
@@ -1,4 +1,10 @@
-simtools
+simtools ![travis-ci](https://travis-ci.org/ZachGlassman/simtools.svg?branch=master)
 --------
 
-![travis-ci](https://travis-ci.org/ZachGlassman/simtools.svg?branch=master)
+simtools provides a tool for simulation pipelines in Python.  Its built to be fairly general and can also perform data analysis.
+
+The package is based on the concept of a Simulation which pipelines different calculations.  These calculations may rely on the previous calculation or be independent.  Some automatic parallelization is performed when possible.
+
+simtools provides easy tooling for computing simulations over a variety of input parameters, organizing out in hdf5 files for further analysis and storage, and a framework for reusable calculation components.
+
+Although originally meant for simulation it can also be used for data analysis.
--- tests/test_Calculation.py
+++ tests/test_Calculation.py
@@ -1,26 +1,27 @@
 import unittest 
 from simtools.Calculation import Calculation
 
+def _f(a, b, c):
+    ans = 0
+    ans2 = []
+    for i in range(1, b*10):
+        ans += a+i/c
+        ans2.append(a*i + c)
+    return {'calc':ans, 'calc2':ans2}
+
+def _f1(calc, calc2):
+    return {'mean': np.mean(calc + calc2),'std':np.std(calc+calc2)}
+
 class test_Calculation(unittest.TestCase):
 
     @classmethod
     def setup_class(self):
         self.plist = [
-            Parameter('a',2,'linspace',(1,6,10)),
-            Parameter('b',30),
-            Parameter('c',2,'arange',(1,10,1))
+            Parameter('a', 2, 'linspace', (1, 6, 10)),
+            Parameter('b', 30),
+            Parameter('c', 2, 'arange', (1, 10, 1))
         ]
         
-        def _f(a,b,c):
-            ans = 0
-            ans2 = []
-            for i in range(1,b*10):
-                ans += a+i/c
-                ans2.append(a*i + c)
-            return {'calc':ans, 'calc2':ans2}
-
-        def _f1(calc, calc2):
-            return {'mean': np.mean(calc + calc2),'std':np.std(calc+calc2)}
             
 
 
